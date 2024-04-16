f70a0ec517bde66911cbfb022fb2c0d6126d9f73
Rbaa02 <raphael.brandaopay@gmail.com>
2024-04-16 11:12:42 -0300

    comentarios

--- app/src/main/java/alvarenga/brandao/lista/activity/MainActivity.java
+++ app/src/main/java/alvarenga/brandao/lista/activity/MainActivity.java
@@ -22,8 +22,12 @@ import alvarenga.brandao.lista.model.MyItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    // ID chamada
     static int NEW_ITEM_REQUEST = 1;
+
+    // lista de itens
     List<MyItem> itens = new ArrayList<>();
+    // cria o myAdapter
     MyAdapter myAdapter;
 
     @Override
@@ -31,42 +35,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // pega o floating button pelo id
         FloatingActionButton fabAddItem = findViewById(R.id.fabAddNewItem);
-
+        //adiciona um ouvidor de clicks
         fabAddItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // cria um intent para a NewItemActivity
                 Intent i = new Intent(MainActivity.this, NewItemActivity.class);
+                // Abre a activity e busca pelo resultado
                 startActivityForResult(i, NEW_ITEM_REQUEST);
             }
         });
 
+        // Pega o Recycler pelo id
         RecyclerView rvItens = findViewById(R.id.rvItens);
 
+        // Cria e seta o myAdapter
         myAdapter = new MyAdapter(this, itens);
         rvItens.setAdapter(myAdapter);
-        
+        // Indica que nao existe variacao de tamanho
         rvItens.setHasFixedSize(true);
-
+        // Cria um gerenciador de layout do tipo linear
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        // Setar ele no RecycleView
         rvItens.setLayoutManager(layoutManager);
-
+        // Criar um decorador para cada item, separando cada um em uma linha
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(rvItens.getContext(), DividerItemDecoration.VERTICAL);
+        // Adiciona o decorador no recycler view
         rvItens.addItemDecoration(dividerItemDecoration);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        // verifica se o ID de request eh igual
         if (requestCode != NEW_ITEM_REQUEST) return;
+        // verifica se abriu sem erros
         if (resultCode != Activity.RESULT_OK) return;
 
+        // cria um novo item
         MyItem myItem = new MyItem();
+        // pega o titulo, descricao e foto do EXTRA e do DATA e adiciona no item
         myItem.title = data.getStringExtra("title");
         myItem.description = data.getStringExtra("description");
         myItem.photo = data.getData();
-
+        // Adiciona o item NOVO na lista de itens
         itens.add(myItem);
+        // Noticica o adapter para mostrar o novo item
         myAdapter.notifyItemInserted(itens.size()-1);
     }
 }
--- app/src/main/java/alvarenga/brandao/lista/activity/NewItemActivity.java
+++ app/src/main/java/alvarenga/brandao/lista/activity/NewItemActivity.java
@@ -18,51 +18,70 @@ import android.widget.Toast;
 import alvarenga.brandao.lista.R;
 
 public class NewItemActivity extends AppCompatActivity {
+    // id da chamada
     static int PHOTO_PICKER_REQUEST = 1;
+
+    // foto selecionada
     Uri photoSelected = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_item);
 
+        // Procurar botao pelo ID
         ImageButton imgCI = findViewById(R.id.imbCl);
+        // Evento de click do botao
         imgCI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Criar uma intent de abrir documento
                 Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                // Somente imagens
                 i.setType("image/*");
+                // Abre a activity e espera um resultado
                 startActivityForResult(i, PHOTO_PICKER_REQUEST);
             }
         });
 
+        // Achar botao pelo id
         Button btnAddItem = findViewById(R.id.btnAddItem);
+        // Adiciona um ouvidor de clicks
         btnAddItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Verifica se selecionou uma imagem
                 if (photoSelected == null) {
                     Toast.makeText(NewItemActivity.this, "É necessário selecionar uma imagem!", Toast.LENGTH_LONG).show();
                     return;
                 }
 
+                // Procura o texto pelo ID e pega o texto
                 EditText etTitle = findViewById(R.id.etTitle);
                 String title = etTitle.getText().toString();
+                // Verifica se o texto e vazio
                 if (title.isEmpty()){
                     Toast.makeText(NewItemActivity.this, "É necessário inserir um título!", Toast.LENGTH_LONG).show();
                     return;
                 }
 
+                // procura a descricao pelo id e pega o texto
                 EditText etDesc = findViewById(R.id.etDesc);
                 String description = etDesc.getText().toString();
+                // Verifica se a descricao e vazio
                 if (description.isEmpty()){
                     Toast.makeText(NewItemActivity.this, "É necessário inserir uma descrição!", Toast.LENGTH_LONG).show();
                     return;
                 }
 
+                // Cria a intent
                 Intent i = new Intent();
+                // Seta as datas e os Extra
                 i.setData(photoSelected);
                 i.putExtra("title", title);
                 i.putExtra("description", description);
                 setResult(Activity.RESULT_OK, i);
+
+                // Finaliza a tarefa
                 finish();
             }
         });
@@ -71,11 +90,16 @@ public class NewItemActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        // verifica se o ID de request eh igual
         if (requestCode != PHOTO_PICKER_REQUEST) return;
+        // verifica se abriu sem erros
         if (resultCode != Activity.RESULT_OK) return;
 
+        // pega a foto do DATA
         photoSelected = data.getData();
+        // pega o imv pelo id
         ImageView imvPhotoPreview = findViewById(R.id.imvfotoPreview);
+        // seta a foto no imageView
         imvPhotoPreview.setImageURI(photoSelected);
     }
 }
